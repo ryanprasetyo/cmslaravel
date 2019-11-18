9252daa4bd4c098e6dafa62347a669d71a98398c
ryan prasetyo <ryanprasetyo107@gmail.com>
2019-11-18 11:00:20 +0700

    category

---
+++ resources/views/categories/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+
+
+<div class="card card-default">
+    <div class="card-header">create category</div>
+        <div class="card-body">
+            <form action="">
+                <div class="form-group"> 
+                    <label for="name"></label>
+                    <input type="text" id="name" class="form-control" name="name">
+                </div>
+
+                <div class="form-group">
+                    
+                     <button class="btn btn-success"> add category</button>  
+                </div>
+
+            </form>
+        </div>
+</div>
+
+
+
+
+
+@endsection
---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+
+<div class="d-flex justify-content-end mb-2">
+
+   <a href="{{ route('categories.create') }}" class="btn btn-success float-right"> Add categories </a>
+</div>
+
+
+
+<div class="card card-default">
+    <div class="card-header">Categories</div>
+</div>
+
+
+
+
+
+@endsection
